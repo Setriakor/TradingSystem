c29b6706f7dcfa8f8e5aefd3767336614ec6b3ac
Setriakor <setriakor@gmail.com>
2020-11-24 11:27:25 +0000

    end to end connectivity done

--- OrderValidation/src/main/java/com/tradingsystem/OrderValidation/OrderValidationController.java
+++ OrderValidation/src/main/java/com/tradingsystem/OrderValidation/OrderValidationController.java
@@ -1,15 +1,20 @@
 package com.tradingsystem.OrderValidation;
 
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import redis.clients.jedis.Jedis;
+
 
 @RestController
 public class OrderValidationController {
-    
+
     @PostMapping("/recieveOrder")
     public String recieveOrder(@RequestBody String order){
-        return order;
+        Jedis jedis = new Jedis("localhost",6379);
+        jedis.publish("orderChannel", order);
+        return "sent";
     }
 }
