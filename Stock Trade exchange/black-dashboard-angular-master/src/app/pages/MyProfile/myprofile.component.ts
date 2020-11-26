2c4cd6c8dd443e9df8a228acb2f1cd4723c27565
amo95 <jameaamo@gmail.com>
2020-11-26 12:03:28 -0500

    CLient UI

---
+++ Stock Trade exchange/black-dashboard-angular-master/src/app/pages/MyProfile/myprofile.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-user",
+  templateUrl: "./myprofile.component.html"
+})
+export class MyProfileComponent implements OnInit {
+  
+constructor() {}
+
+  ngOnInit() {}
+}
